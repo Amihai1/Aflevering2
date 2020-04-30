3a3289302a2dcd32376e7c20903165c3d47ba964
kevinbpedersen1 <61202791+kevinbpedersen1@users.noreply.github.com>
2020-04-30 17:32:37 +0200

    Merge branch 'master' of /Users/kevinpedersen/IdeaProjects/Aflevering2 with conflicts.

--- src/main/java/EKG/DataController.java
+++ src/main/java/EKG/DataController.java
@@ -8,7 +8,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.application.Application;
-
 import java.io.IOException;
 
 
@@ -20,30 +19,29 @@ public class DataController implements DataListener {
 
 
     public void buttonPressed(ActionEvent actionEvent) {
-        DataObservable DataStation = new PatientDataGenerator();
-        new Thread(DataStation).start();
-        DataStation.register(this);
+    DataObservable DataStation =new PatientDataGenerator();
+    new Thread(DataStation).start();
+    DataStation.register(this);
+    }
+    public void DataRecord(ActionEvent actionEvent) {this.record = !this.record;
     }
 
-    public void DataRecord(ActionEvent actionEvent) {
-        this.record = !this.record;
-    }
-
-
+    
     @Override
     public void notify(PatientDTO data) {
-        String text = DataOutput.getText();
-        text += "Time: " + data.getTime() + "Temperatur: " + data.getTemp() + "SpO2: " + data.getSpO2() + "BPM: " + data.getBPM() + "\r\n";
-        DataOutput.setText(text);
-        if (this.record) {
-            data.setPatientId(DataField.getText());
-            sampleReader.save(data);
-        }
+    String text = DataOutput.getText();
+    text += "Time: "+data.getTime() + "Temperatur: "+ data.getTemp() + "SpO2: " + data.getSpO2() + "BPM: "+ data.getBPM() + "\r\n";
+    DataOutput.setText(text);
+
+    if(this.record){
+        data.setPatientId(DataField.getText());
+        sampleReader.save(data);
+    }
     }
 
 
     public void LoadData(ActionEvent actionEvent) {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/LoadGui.fxml"));
+        FXMLLoader fxmlLoader =new FXMLLoader(getClass().getResource("/LoadGui.fxml"));
         try {
             FlowPane flowPane = fxmlLoader.load();
             Stage loadStage = new Stage();
@@ -54,5 +52,5 @@ public class DataController implements DataListener {
         }
     }
 
-
+    
 }
